7c38e27f1d0644c4344403102a4887cee92a46e1
Jahde <jahde@me.com>
2014-11-18 09:20:49 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+
+var myName = "Jahde";
+letterColors = [red, orange, green, blue, purple]
+
+var num = 5;
+
+if (num < 10) {
+    bubbleShape = "circle";
+}
+
+else {
+    bubbleShape = "square";
+}
+
+
+drawName(myName, letterColors);
+bounceBubbles()
